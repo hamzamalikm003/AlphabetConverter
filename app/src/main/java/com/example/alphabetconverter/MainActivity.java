1639daa0ff03646b87992647ac69a3de6cc84906
Hamza Malik <hamza.malik@frag-games.com>
2021-10-23 17:47:38 +0500

    first commit

---
+++ app/src/main/java/com/example/alphabetconverter/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.alphabetconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText edittext;
+    private Button btn;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edittext=findViewById(R.id.InputField);
+        btn=findViewById(R.id.submit);
+        textView=findViewById(R.id.result);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Character.isLowerCase(edittext.getText().toString().charAt(0))){
+                    textView.setText(Character.toString(Character.toUpperCase(edittext.getText().toString().charAt(0))));
+                }
+                else {
+                    textView.setText(Character.toString(Character.toLowerCase(edittext.getText().toString().charAt(0))));
+                }
+            }
+        });
+    }
+
+}
